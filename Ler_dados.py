87ac0ffb4a02e50ef6f733cde2391e896cb84612
emersonsilva0 <116392348+emersonsilva0@users.noreply.github.com>
2024-02-01 10:09:22 -0300

    Update Ler_dados.py

--- Ler_dados.py
+++ Ler_dados.py
@@ -4,11 +4,14 @@ def ler_dados(nome_arquivo):
     # Lê o arquivo como um DataFrame do pandas e converte diretamente para uma matriz (lista de listas)
     return pd.read_csv(nome_arquivo, header=None, delim_whitespace=True).values.tolist()
 
-# Bases de dados a serem consideradas
+# Bases de dados a ser usadas
 bases_de_dados = ["ATT48", "DANTZIG42", "FRI26", "GR17", "P01"]
 
-# Carregar cada base de dados e armazenar as matrizes em um dicionário
-matrizes_bases = {base: ler_dados(f'{base.lower()}.txt') for base in bases_de_dados}
+# Caminho dos arquivos 
+caminho =  "C:\\Users\\Emersontntky\\TG\\Base de Dados\\"
+
+# Coloca cada base de dados e armazenar as matrizes em um dicionário
+matrizes_bases = {base: ler_arquivo_para_matriz(caminho + f'{base.lower()}.txt') for base in bases_de_dados}
 
 # Imprimir as matrizes de cada base de dados
 for base, matriz in matrizes_bases.items():
